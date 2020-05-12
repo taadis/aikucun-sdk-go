56fe618b49e935e35b7147e010be069e0fb654f8
taadis <i@taadis.com>
2020-05-12 14:52:50 +0800

    update client

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/taadis/aikucun-sdk-go
 
 go 1.11
+
+require (
+	github.com/google/go-querystring v1.0.0
+	golang.org/x/net v0.0.0-20200506145744-7e3656a0809f // indirect
+)
--- sdk/client.go
+++ sdk/client.go
@@ -1,7 +1,11 @@
 package sdk
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
+
+	"github.com/google/go-querystring/query"
 )
 
 // 客户端结构体
@@ -21,5 +25,31 @@ func NewClient(options ...Option) *Client {
 
 // Do
 func (client *Client) Do(request IRequest, response IResponse) (err error) {
+	url := client.options.Url + request.Path()
+	method := request.Method()
+	req, err := http.NewRequest(method, url, nil)
+	if err != nil {
+		return err
+	}
+
+	// 如何拼接动态结构体的键值
+	v, _ := query.Values(request)
+	req.URL.RawQuery = v.Encode()
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	res, err := client.httpClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
+	if err = json.Unmarshal(
+		body,     // data []byte,
+		response, // v interface{}
+	); err != nil {
+		return err
+	}
 	return
 }
