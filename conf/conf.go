ad7f1efe787f27b6790b489fc4fd5a268b8da33e
taadis <i@taadis.com>
2020-06-29 20:47:47 +0800

    update conf

--- conf/conf.go
+++ conf/conf.go
@@ -1,6 +1,27 @@
 package conf
 
-var (
-	TestUrl string = "https://openapi.akucun.com"
-	ProUrl  string = "https://openapi.aikucun.com"
+import (
+	"sync"
 )
+
+var (
+	once sync.Once
+	conf *Conf
+)
+
+// Conf
+type Conf struct {
+	TestUrl string
+	ProUrl  string
+}
+
+// Conf
+func GetConf() *Conf {
+	once.Do(func() {
+		conf = &Conf{
+			TestUrl: "https://openapi.akucun.com",
+			ProUrl:  "https://openapi.aikucun.com",
+		}
+	})
+	return conf
+}
