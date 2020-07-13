053758421f78e4069cb5f5c7bdbff8754d8368bb
taadis <i@taadis.com>
2020-07-13 14:59:15 +0800

    update sign

--- sdk/util.go
+++ sdk/util.go
@@ -1,25 +1,56 @@
 package sdk
 
 import (
-	"encoding/json"
+	"crypto/sha1"
+	"fmt"
 	"log"
-	"time"
+	"sort"
 )
 
 // GetSign
-func GetSign(appId string, appSecret string, nonceStr string, erp string, erpVersion string, timestamp string) (string, error) {
+func GetSign(appId string, appSecret string, nonceStr string, erp string, erpVersion string, timestamp string, queryParams map[string]interface{}, postJson string) (string, error) {
 	obj := make(map[string]interface{})
 	obj["appid"] = appId
 	obj["appsecret"] = appSecret
 	obj["noncestr"] = nonceStr
-	obj["timestamp"] = time.Now().Unix()
+	obj["timestamp"] = timestamp //time.Now().Unix()
 	obj["erp"] = erp
 	obj["erpversion"] = erpVersion
 	log.Println(obj)
 
-	jsonBytes, err := json.Marshal(obj)
-	if err != nil {
-		return "", err
+	// add queryParams
+	if queryParams != nil {
+		log.Println("queryParams != nil 添加 GET 查询参数")
+		for key, value := range queryParams {
+			obj[key] = value
+		}
+		log.Println("添加 GET 查询参数后的对象为:", obj)
 	}
-	return string(jsonBytes), nil
+
+	// add postJson
+	if postJson != "" {
+		log.Println("postJson 不为空, 准备添加 POST 查询参数")
+		obj["post"] = postJson
+		log.Println("添加 POST 查询参数后的对象为:", obj)
+	}
+
+	// sort
+	var keys []string
+	for key := range obj {
+		keys = append(keys, key)
+	}
+	sort.Strings(keys)
+	str := ""
+	for _, key := range keys {
+		if str != "" {
+			str += fmt.Sprint("&", key, "=", obj[key].(string))
+		} else {
+			str += fmt.Sprint(key, "=", obj[key].(string))
+		}
+	}
+	log.Println("加密前的字符串:", str)
+
+	// sha1
+	ret := fmt.Sprintf("%x", sha1.New().Sum([]byte(str)))
+	return ret, nil
 }
--- sdk/util_test.go
+++ sdk/util_test.go
@@ -6,6 +6,11 @@ import (
 
 // TestGetSign
 func TestGetSign(t *testing.T) {
-	appId := "appId"
-	sign := GetSign(appId)
+	var appId, appSecret, nonceStr, erp, erpVersion, timestamp, postJson string
+	queryParams := make(map[string]interface{})
+	sign, err := GetSign(appId, appSecret, nonceStr, erp, erpVersion, timestamp, queryParams, postJson)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	t.Log("sign=", sign)
 }
