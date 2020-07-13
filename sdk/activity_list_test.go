296cf4ff2205ff0c7d6db97ec6b7a3162852458e
taadis <i@taadis.com>
2020-07-13 13:52:53 +0800

    update activity list test

--- sdk/activity_list_test.go
+++ sdk/activity_list_test.go
@@ -7,7 +7,8 @@ import (
 )
 
 func TestActivityList(t *testing.T) {
-	client := NewClient(WithUrl(conf.TestUrl), WithAppId(""), WithAppSecret(""))
+	c := conf.GetConf()
+	client := NewClient(WithUrl(c.TestUrl), WithAppId(""), WithAppSecret(""))
 	res, err := client.ActivityList(nil)
 	t.Log("err:", err.Error())
 	if err != nil {
