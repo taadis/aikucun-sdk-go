43a43ef11a54f6c43abceb85785179bcba7ae034
taadis <i@taadis.com>
2020-07-14 10:23:19 +0800

    add test args

--- sdk/client_test.go
+++ sdk/client_test.go
@@ -1,1 +1,21 @@
 package sdk
+
+import (
+	"flag"
+	"testing"
+)
+
+var (
+	appId      = flag.String("appId", "", "appId")
+	appSecret  = flag.String("appSecret", "", "appSecret")
+	erp        = flag.String("erp", "", "erp")
+	erpVersion = flag.String("erpVersion", "", "erpVersion")
+)
+
+// parseArgs
+func parseArgs(t *testing.T) {
+	if !flag.Parsed() {
+		flag.Parse()
+	}
+	t.Logf("-args -appId %s -appSecret %s", *appId, *appSecret)
+}
