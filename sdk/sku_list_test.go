59c3ed37a18c428c538342ddeda3c9fb7b592aef
taadis <i@taadis.com>
2020-07-16 17:25:24 +0800

    update sku list test

--- sdk/sku_list_test.go
+++ sdk/sku_list_test.go
@@ -1,1 +1,28 @@
 package sdk
+
+import (
+	"testing"
+
+	"gitee.com/taadis/aikucun-sdk-go/conf"
+)
+
+func TestSkuList(t *testing.T) {
+	parseArgs(t)
+	client := NewClient(
+		WithUrl(conf.UrlTest),
+		WithAppId(*appId),
+		WithAppSecret(*appSecret),
+		WithErp(*erp),
+		WithErpVersion(*erpVersion),
+	)
+	req := &SkuListRequest{
+		ActivityId: "",
+		Page:       1,
+		PageSize:   2,
+	}
+	resp, err := client.SkuList(req)
+	if err != nil {
+		t.Fatal(".SkuList() error:", err.Error())
+	}
+	t.Log(resp)
+}
