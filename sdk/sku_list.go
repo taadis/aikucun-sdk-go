1faf42a41e907034c498757dae501779ce66e9cd
taadis <i@taadis.com>
2020-07-16 13:10:28 +0800

    update sku list

--- sdk/sku_list.go
+++ sdk/sku_list.go
@@ -1,5 +1,10 @@
 package sdk
 
+import (
+	"net/http"
+	"strconv"
+)
+
 // SkuListRequest
 type SkuListRequest struct {
 	ActivityId string `json:"activityid"`
@@ -9,11 +14,47 @@ type SkuListRequest struct {
 
 // SkuListResponse
 type SkuListResponse struct {
+	BaseResponse
+	SkuListResponseData struct {
+		TotalRecord int `json:"totalrecord"`
+		PageSize    int `json:"pagesize"`
+		TotalPage   int `json:"totalpage"`
+		Page        int `json:"page"`
+		List        []struct {
+			Id        string `json:"id"`
+			BarCode   string `json:"barcode"`
+			KuanHao   string `json:"kuanhao"`
+			BrandId   string `json:"brandid"`
+			BrandName string `json:"brandname"`
+			Name      string `json:"name"`
+			Size      string `json:"size"`
+			Color     string `json:"color"`
+		}
+	}
+}
+
+// Method
+func (req *SkuListRequest) Method() string {
+	return http.MethodGet
+}
+
+// Path
+func (req *SkuListRequest) Path() string {
+	return "/api/v2/sku/list"
+}
+
+// Params
+func (req *SkuListRequest) Params() map[string]string {
+	m := make(map[string]string)
+	m["activityid"] = req.ActivityId
+	m["page"] = strconv.Itoa(req.Page)
+	m["pagesize"] = strconv.Itoa(req.PageSize)
+	return m
 }
 
 // SkuList
 func (client *Client) SkuList(request *SkuListRequest) (response *SkuListResponse, err error) {
 	response = &SkuListResponse{}
-	// err = client.Do(request, response)
+	err = client.Do(request, response)
 	return
 }
