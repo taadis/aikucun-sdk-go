297beef22bf5f5d44ebb5458225e68f65c960cea
taadis <i@taadis.com>
2020-07-18 11:03:22 +0800

    update delivery query

--- sdk/delivery_query.go
+++ sdk/delivery_query.go
@@ -1,5 +1,9 @@
 package sdk
 
+import (
+	"net/http"
+)
+
 // DeliveryQueryRequest
 type DeliveryQueryRequest struct {
 	AdorderId string `json:"adorderid"`
@@ -7,11 +11,61 @@ type DeliveryQueryRequest struct {
 
 // DeliveryQueryResponse
 type DeliveryQueryResponse struct {
+	BaseResponse
+	Data DeliveryQueryData `json:"data"`
+}
+
+// DeliveryQueryData
+type DeliveryQueryData struct {
+	DeliverNos           []string `json:"deliverNos"`
+	LogisticsCompany     int      `json:"logisticsCompany"`
+	InsuranceValue       int      `json:"insuranceValue"`
+	BackSignBill         string   `json:"backSignBill"`
+	CodValue             int      `json:"codValue"`
+	Province             string   `json:"province"`
+	City                 string   `json:"city"`
+	County               string   `json:"county"`
+	ReceiverArea         string   `json:"receiverArea"`
+	Receiver             string   `json:"receiver"`
+	ReceiverTel          string   `json:"receiverTel"`
+	ReceiverAddress      string   `json:"receiverAddress"`
+	Sender               string   `json:"sender"`
+	SendTel              string   `json:"sendTel"`
+	SendAddress          string   `json:"sendAddress"`
+	Remark               string   `json:"remark"`
+	SectionCode          string   `json:"sectionCode"`
+	SourceSortCenterName string   `json:"sourceSortCenterName"`
+	TargetSortCenterName string   `json:"targetSortCenterName"`
+	DestinationCity      string   `json:"destinationCity"`
+	LogisticsAreaCode    string   `json:"logisticsAreaCode"`
+	Col1                 string   `json:"col1"`
+	Col2                 string   `json:"col2"`
+	Col3                 string   `json:"col3"`
+	Col4                 string   `json:"col4"`
+	Col5                 string   `json:"col5"`
+}
+
+// Method
+func (req *DeliveryQueryRequest) Method() string {
+	return http.MethodGet
+}
+
+// Path
+func (req *DeliveryQueryRequest) Path() string {
+	return "/api/v2/delivery/query"
+}
+
+// Params
+func (req *DeliveryQueryRequest) Params() map[string]interface{} {
+	m := make(map[string]interface{})
+	m["adorderid"] = req.AdorderId
+	m["version"] = 2
+	return m
 }
 
 // DeliveryQuery
 func (client *Client) DeliveryQuery(request *DeliveryQueryRequest) (response *DeliveryQueryResponse, err error) {
 	response = &DeliveryQueryResponse{}
-	// TODO:
+	err = client.Do(request, response)
 	return
 }
--- sdk/delivery_query_test.go
+++ sdk/delivery_query_test.go
@@ -2,9 +2,28 @@ package sdk
 
 import (
 	"testing"
+
+	"gitee.com/taadis/aikucun-sdk-go/conf"
 )
 
 // TestDeliveryQuery
 func TestDeliveryQuery(t *testing.T) {
-
+	parseArgs(t)
+	client := NewClient(
+		WithUrl(conf.UrlTest),
+		WithAppId(*appId),
+		WithAppSecret(*appSecret),
+		WithErp(*erp),
+		WithErpVersion(*erpVersion),
+	)
+	req := &DeliveryQueryRequest{
+		AdorderId: "",
+	}
+	resp, err := client.DeliveryQuery(req)
+	if err != nil {
+		t.Fatal(".DeliveryQuery error:", err.Error())
+	}
+	if resp.Code != 0 {
+		t.Fatal(".DeliveryQuery fail:", resp.Message)
+	}
 }
