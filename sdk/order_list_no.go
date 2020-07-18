83721f0f084a277c5865e9ded4463ca27d2b78f2
taadis <i@taadis.com>
2020-07-18 13:36:43 +0800

    update order list no

--- sdk/order_list_no.go
+++ sdk/order_list_no.go
@@ -1,5 +1,9 @@
 package sdk
 
+import (
+	"net/http"
+)
+
 // OrderListNoRequest
 type OrderListNoRequest struct {
 	ActivityId    string `json:"activityid"`
@@ -10,11 +14,55 @@ type OrderListNoRequest struct {
 
 // OrderListNoResponse
 type OrderListNoResponse struct {
+	BaseResponse
+	Data OrderListNoData `json:"data"`
+}
+
+// OrderListNoData
+type OrderListNoData struct {
+	TotalRecord int               `json:"totalrecord"`
+	TotalPage   int               `json:"totalpage"`
+	Page        int               `json:"page"`
+	PageSize    int               `json:"pagesize"`
+	Source      string            `json:"source"`
+	ActivityId  string            `json:"activityid"`
+	List        []OrderListNoItem `json:"list"`
+}
+
+// OrderListNoItem
+type OrderListNoItem struct {
+	AdorderId                 string `json:"adorderid"`
+	RealSettlementpriceAmount int    `json:"realSettlementpriceAmount"`
+	Count                     int    `json:"count"`
+	OrderStatus               int    `json:"orderstatus"`
+	LogisticsStatus           int    `json:"logisticsstatus"`
+	OrderTime                 string `json:"ordertime"`
+	PayTime                   string `json:"paytime"`
+}
+
+// Method
+func (req *OrderListNoRequest) Method() string {
+	return http.MethodGet
+}
+
+// Path
+func (req *OrderListNoRequest) Path() string {
+	return "/api/v2/order/listno"
+}
+
+// Params
+func (req *OrderListNoRequest) Params() map[string]interface{} {
+	m := make(map[string]interface{})
+	m["activityid"] = req.ActivityId
+	m["page"] = req.Page
+	m["pagesize"] = req.PageSize
+	m["filterDeliver"] = req.FilterDeliver
+	return m
 }
 
 // OrderListNo
 func (client *Client) OrderListNo(request *OrderListNoRequest) (response *OrderListNoResponse, err error) {
 	response = &OrderListNoResponse{}
-	// TODO:
+	err = client.Do(request, response)
 	return
 }
--- sdk/order_list_no_test.go
+++ sdk/order_list_no_test.go
@@ -2,9 +2,31 @@ package sdk
 
 import (
 	"testing"
+
+	"gitee.com/taadis/aikucun-sdk-go/conf"
 )
 
 // TestOrderListNo
 func TestOrderListNo(t *testing.T) {
-
+	parseArgs(t)
+	client := NewClient(
+		WithUrl(conf.UrlTest),
+		WithAppId(*appId),
+		WithAppSecret(*appSecret),
+		WithErp(*erp),
+		WithErpVersion(*erpVersion),
+	)
+	req := &OrderListNoRequest{
+		ActivityId:    "",
+		Page:          1,
+		PageSize:      2,
+		FilterDeliver: 0,
+	}
+	resp, err := client.OrderListNo(req)
+	if err != nil {
+		t.Fatal(".OrderListNo error:", err.Error())
+	}
+	if resp.Code != 0 {
+		t.Fatal(".OrderListNo resp message:", resp.Message)
+	}
 }
