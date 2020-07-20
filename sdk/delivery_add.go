8f3d6efa79c9cf18d26cc3d9de9737bc6e2e8bc8
taadis <i@taadis.com>
2020-07-20 11:26:31 +0800

    update delivery add

--- sdk/delivery_add.go
+++ sdk/delivery_add.go
@@ -1,5 +1,10 @@
 package sdk
 
+import (
+	//"encoding/json"
+	"net/http"
+)
+
 // DeliveryAddRequest
 type DeliveryAddRequest struct {
 	AdorderId string `json:"adorderid"`
@@ -7,11 +12,36 @@ type DeliveryAddRequest struct {
 
 // DeliveryAddResponse
 type DeliveryAddResponse struct {
+	BaseResponse
+}
+
+// Method
+func (req *DeliveryAddRequest) Method() string {
+	return http.MethodPost
+}
+
+// Path
+func (req *DeliveryAddRequest) Path() string {
+	return "/api/v2/delivery/add"
+}
+
+// Params
+func (req *DeliveryAddRequest) Params() map[string]interface{} {
+	/*
+		bs, err := json.Marshal(req)
+		if err != nil {
+			panic(err)
+		}
+	*/
+	m := make(map[string]interface{})
+	//m["verison"] = 2
+	//m["adorderid"] = string(bs[:])
+	return m
 }
 
 // DelievryAdd
-func (this *Client) DeliveryAdd(request *DeliveryAddRequest) (response *DeliveryAddResponse, err error) {
+func (client *Client) DeliveryAdd(request *DeliveryAddRequest) (response *DeliveryAddResponse, err error) {
 	response = &DeliveryAddResponse{}
-	// TODO:
+	err = client.Do(request, response)
 	return
 }
--- sdk/delivery_add_test.go
+++ sdk/delivery_add_test.go
@@ -2,9 +2,28 @@ package sdk
 
 import (
 	"testing"
+
+	"gitee.com/taadis/aikucun-sdk-go/conf"
 )
 
 // TestDeliveryAdd
 func TestDeliveryAdd(t *testing.T) {
-
+	parseArgs(t)
+	client := NewClient(
+		WithUrl(conf.UrlTest),
+		WithAppId(*appId),
+		WithAppSecret(*appSecret),
+		WithErp(*erp),
+		WithErpVersion(*erpVersion),
+	)
+	req := &DeliveryAddRequest{
+		AdorderId: "xxx",
+	}
+	resp, err := client.DeliveryAdd(req)
+	if err != nil {
+		t.Fatal(".DeliveryAdd error:", err.Error())
+	}
+	if resp.Code != 0 {
+		t.Fatal(".DeliveryAdd resp fail:", resp.Message)
+	}
 }
