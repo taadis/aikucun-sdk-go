b7989be11524be80e785236a4cae723c31cc2fdf
taadis <i@taadis.com>
2020-07-20 14:18:59 +0800

    update delivery add

--- sdk/delivery_add.go
+++ sdk/delivery_add.go
@@ -13,6 +13,37 @@ type DeliveryAddRequest struct {
 // DeliveryAddResponse
 type DeliveryAddResponse struct {
 	BaseResponse
+	Data DeliveryAddData `json:"data"`
+}
+
+// DeliveryAddData
+type DeliveryAddData struct {
+	DeliveryNo           string `json:"deliverNo"`
+	LogisticsCompany     int    `json:"logisticsCompany"`
+	InsuranceValue       int    `json:"insuranceValue"`
+	BackSignBill         string `json:"backSignBill"`
+	CodValue             int    `json:"codValue"`
+	Province             string `json:"province"`
+	City                 string `json:"city"`
+	County               string `json:"county"`
+	ReciverArea          string `json:"receiverArea"`
+	Receiver             string `json:"receiver"`
+	ReceiverTel          string `json:"receiverTel"`
+	ReceiverAddress      string `json:"receiverAddress"`
+	Sender               string `json:"sender"`
+	SendTel              string `json:"sendTel"`
+	SendAddress          string `json:"sendAddress"`
+	Remark               string `json:"remark"`
+	SectionCode          string `json:"sectionCode"`
+	SourceSortCenterName string `json:"sourceSortCenterName"`
+	TargetSortCenterName string `json:"targetSortCenterName"`
+	DestinationCity      string `json:"destinationCity"`
+	LogisticsAreaCode    string `json:"logisticsAreaCode"`
+	Col1                 string `json:"col1"`
+	Col2                 string `json:"col2"`
+	Col3                 string `json:"col3"`
+	Col4                 string `json:"col4"`
+	Col5                 string `json:"col5"`
 }
 
 // Method
