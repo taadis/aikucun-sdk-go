871efd850eb880fb881e0a53888de565645c28ba
taadis <i@taadis.com>
2020-07-21 14:00:51 +0800

    update invoice upload

--- sdk/invoice_upload.go
+++ sdk/invoice_upload.go
@@ -1,16 +1,62 @@
 package sdk
 
+import (
+	"net/http"
+)
+
 // InvoiceUploadRequest
 type InvoiceUploadRequest struct {
+	ActivityId string               `json:"activityid"`
+	Size       int                  `json:"size"`
+	List       []InvoiceUploadOrder `json:"list"`
+}
+
+// InvoiceUploadOrder
+type InvoiceUploadOrder struct {
+	AdorderId        string                 `json:"adorderid"`
+	ErpOrderId       string                 `json:"erporderid"`
+	DeliverNo        string                 `json:"deliverNo"`
+	Status           int                    `json:"status"`
+	Size             int                    `json:"size"`
+	List             []InvoiceUploadProduct `json:"list"`
+	LogisticsCompany string                 `json:"logisticsCompany"`
+}
+
+// InvoiceUploadProduct
+type InvoiceUploadProduct struct {
+	PinPai  string `json:"pinpai"`
+	BarCode string `json:"barcode"`
+	KuanHao string `json:"kuanhao"`
+	Num     int    `json:"num"`
+	RealNum int    `json:"realnum"`
+	LackNum int    `json:"lacknum"`
 }
 
 // InvoiceUploadResponse
 type InvoiceUploadResponse struct {
+	BaseResponse
+}
+
+// Method
+func (req *InvoiceUploadRequest) Method() string {
+	return http.MethodPost
+}
+
+// Path
+func (req *InvoiceUploadRequest) Path() string {
+	return "/api/v2/invoice/upload"
+}
+
+// Params
+func (req *InvoiceUploadRequest) Params() map[string]interface{} {
+	m := make(map[string]interface{})
+	m["version"] = 2
+	return m
 }
 
 // InvoiceUpload
-func (this *Client) InvoiceUpload(request *InvoiceUploadRequest) (response *InvoiceUploadResponse, err error) {
+func (client *Client) InvoiceUpload(request *InvoiceUploadRequest) (response *InvoiceUploadResponse, err error) {
 	response = &InvoiceUploadResponse{}
-	// TODO:
+	err = client.Do(request, response)
 	return
 }
--- sdk/invoice_upload_test.go
+++ sdk/invoice_upload_test.go
@@ -2,9 +2,26 @@ package sdk
 
 import (
 	"testing"
+
+	"gitee.com/taadis/aikucun-sdk-go/conf"
 )
 
 // TestInvoiceUpload
 func TestInvoiceUpload(t *testing.T) {
-
+	parseArgs(t)
+	client := NewClient(
+		WithUrl(conf.UrlTest),
+		WithAppId(*appId),
+		WithAppSecret(*appSecret),
+		WithErp(*erp),
+		WithErpVersion(*erpVersion),
+	)
+	req := &InvoiceUploadRequest{}
+	resp, err := client.InvoiceUpload(req)
+	if err != nil {
+		t.Fatal(".InvoiceUpload error:", err.Error())
+	}
+	if resp.Code != 0 {
+		t.Fatal(".InvoiceUpload resp fail:", resp.Message)
+	}
 }
